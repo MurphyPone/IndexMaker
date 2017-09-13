e307b9d4e2207278973ecb86154ae43e6193fd4f
MurphyP1 <MurphyP1@104-10-2017.bi.local>
2017-09-13 15:08:36 -0400

    8:08 :((((

--- IndexMaker/src/DocumentIndex.java
+++ IndexMaker/src/DocumentIndex.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
 
-public class DocumentIndex {
+public class DocumentIndex extends ArrayList<IndexEntry>{
+	
+	public Documentindex() {
+		
+	}
+	
+	public DocumentIndex(int size) {
+		
+	}
+	
+	
+	public void addWord(String word, int num) { 
+		new IndexEntry(word);
+		
+	}
 
 }
